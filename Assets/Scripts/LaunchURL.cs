ee05070612253c79085b9772b73d1a1dfc705b0c
FatalGameStudios <36797676+FatalGameStudios@users.noreply.github.com>
2018-03-05 20:36:24 -0600

    Add files via upload

---
+++ Assets/Scripts/LaunchURL.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchURL : MonoBehaviour {
+
+	public string URL; 
+	public string URL2;
+	public string URL3;
+
+	public void urlLinkOrWeb() 
+	{
+		Application.OpenURL (URL);
+	}
+
+	public void urlLinkOrWeb2() 
+	{
+		Application.OpenURL (URL2);
+	}
+
+	public void urlLinkOrWeb3() {
+		Application.OpenURL (URL3);
+	}
+}
