0d2691f3fbb628af83cfbd650972b69bd4efee62
FatalGameStudios <36797676+FatalGameStudios@users.noreply.github.com>
2018-03-05 20:33:48 -0600

    Add files via upload

---
+++ Assets/LeanPeekSystem/Scripts/peek.js
@@ -0,0 +1,54 @@
+﻿var cam : Transform;
+
+private var nextPos : float;
+
+private var nextPos2 : float;
+
+private var dampVelocity : float;
+
+private var dampVelocity2 : float;
+
+
+
+
+function Update () {
+
+    var newPos = Mathf.SmoothDamp(cam.transform.localPosition.x, nextPos, dampVelocity, 0.2);
+
+    var newPos2 = Mathf.SmoothDamp(cam.transform.localPosition.y, nextPos2, dampVelocity2, 0.2);
+
+    cam.transform.localPosition.x = newPos;
+
+    cam.transform.localPosition.y = newPos2;
+
+    if(Input.GetKey("e")){
+        
+        //this value is the distance your camere goes right when you peek
+        nextPos = 0.25;
+
+        nextPos2 = 0.0;
+    
+        
+
+    }else if(Input.GetKey("q")){
+
+        //this value is the distance your camere goes left when you peek
+        nextPos = -0.25;
+
+        nextPos2 = 0.0;
+        
+
+    }else if(Input.GetKey("left ctrl")){  
+
+        nextPos = 0;
+
+        nextPos2 = -0.45;
+        
+
+    }else{
+        nextPos = 0.0;
+
+        nextPos2 = 0;
+        
+    }
+}
---
+++ Assets/LeanPeekSystem/Tutorial.js
@@ -0,0 +1,16 @@
+﻿/*IMPORTANT: You need to install Standard Assets_Characters for the demo scene to work!
+
+Tutorial: If you are using the fps controller, you have to set up some things. First you have to make a new layer for your weapon.
+Next up you need to change te first person character's camera, so it can't see the weapon layer. (Culling mask) 
+
+After you've done that, you need to set up 2 camera's inside the first person character. Let's call the first one camera, and te second one camera02. 
+On the first camera you need to set the culling mask to weapon only, and on camera02 to everything except the weapon layer. 
+the first camera is depth only, min clipping planes 0.001, and depth 1. camera02 is skybox, and clipping planes and depth the same as the first camera. 
+You have to apply the two scripts to the first person character. Now you have to apply the first camera to the peek script, and the second camera to the lean script.
+
+If you want to change the controls, go to the scripts, and inside you will find: if input.getkey. There you can change the input to what you want. 
+
+If you want to change how much the camera goes left or right, go inside the peek script and change the nextPos under input q and e. 
+
+if you want to change how much your weapon rotates, go to the lean script inside the inspector and change the values to whatever you want.
+*/
